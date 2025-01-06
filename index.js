2b075d5dd7ee91299c2aa2e6d73495298fee88bb
Zakarias Broberg-Jepsen <zbj@ipwsystems.dk>
2025-01-06 14:30:10 +0100

    Back to render

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ async function handleFileUpload(event) {
 
         try {
             startLoading();
-            const response = await fetch('https://hetzner-dev-ws.ipwsystems.dk/upload', {
+            const response = await fetch('https://rizzapp-server.onrender.com/upload', {
                 method: 'POST',
                 body: formData,
             });
@@ -107,7 +107,7 @@ tokenInput.addEventListener('keypress', (event) => {
 });
 
 function getUsesLeft() {
-    fetch('https://hetzner-dev-ws.ipwsystems.dk/uses', {
+    fetch('https://rizzapp-server.onrender.com/uses', {
         method: 'POST',
         body: JSON.stringify({ token: localStorage.getItem('token') }),
         headers: {
